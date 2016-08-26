3473feef0b57d502aaa658d79b9ad588f08f0a2d
mari44ka <marikostenko83@gmail.com>
2016-08-25 23:04:24 -0700

    creating methods

--- add_group.py
+++ add_group.py
@@ -20,16 +20,16 @@ class add_group(unittest.TestCase):
     def test_add_group(self):
 
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_css_selector("html").click()
-        wd.find_element_by_css_selector("html").click()
+        self.open_homepage(wd)
+        self.login(wd)
+        #creating group
+        self.group_creating(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def group_creating(self, wd):
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
@@ -42,8 +42,21 @@ class add_group(unittest.TestCase):
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys("my")
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def login(self, wd):
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_css_selector("html").click()
+        wd.find_element_by_css_selector("html").click()
+
+    def open_homepage(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
 
     def tearDown(self):
         self.wd.quit()
