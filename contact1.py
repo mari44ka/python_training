744c14651d66f7b4ba13308321b869ab1e5c2a0b
mari44ka <marikostenko83@gmail.com>
2016-08-26 23:45:43 -0700

    creating class Contact1

---
+++ contact1.py
@@ -0,0 +1,21 @@
+class Contact1:
+    def __init__(self,fname, mname, lname, nname, title, compname, address,
+                 hphone, mphone, wphone, faxnumber, e_mail, e_mail2, e_mail3, home_page, birth_year, address_2, notes):
+        self.fname=fname
+        self.mname=mname
+        self.lname=lname
+        self.nname=nname
+        self.title=title
+        self.compname=compname
+        self.address=address
+        self.hphone=hphone
+        self.mphone=mphone
+        self.wphone=wphone
+        self.faxnumber=faxnumber
+        self.e_mail=e_mail
+        self.e_mail2=e_mail2
+        self.e_mail3=e_mail3
+        self.home_page=home_page
+        self.birth_year=birth_year
+        self.address_2=address_2
+        self.notes=notes
