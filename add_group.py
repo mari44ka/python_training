8c96e7d4c6fe0af88c2d6bb1bc09acadb00c889e
mari44ka <marikostenko83@gmail.com>
2016-09-01 00:41:57 -0700

    creating fixture for using pytest

--- add_group.py
+++ add_group.py
@@ -1,43 +1,30 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
 from group import Group
-import  unittest
 from Application import Application
+import pytest
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture=Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class add_group(unittest.TestCase):
-    def setUp(self):
-        self.app=Application()
 
-    def test_add_group(self):
-        self.app.open_homepage()
-        self.app.login(name="admin", password="secret")
-        #creating group
-        self.app.group_creating(Group(name="my", footer="my", header="my"))
-        self.app.logout()
+def test_add_group(app):
+        app.open_homepage()
+        app.login(name="admin", password="secret")
+        app.group_creating(Group(name="my", footer="my", header="my"))
+        app.logout()
 
-    def test_add_group_empty(self):
-
-        self.app.open_homepage()
-        self.app.login(name="admin", password="secret")
-
-            # creating group
-        self.app.group_creating(Group(name="", footer="", header=""))
-
-        self.app.logout()
+def test_add_group_empty(app):
+        app.open_homepage()
+        app.login(name="admin", password="secret")
+        app.group_creating(Group(name="", footer="", header=""))
+        app.logout()
 
 
-    def tearDown(self):
-        self.app.destroy()
 
 
-if __name__ == '__main__':
-    unittest.main()
+
