f26c078bf93bbe4dc7e6584249303eadd0ae2898
mari44ka <marikostenko83@gmail.com>
2016-09-02 00:10:26 -0700

    using new class Contact

--- add_new_contact.py
+++ add_new_contact.py
@@ -3,6 +3,7 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 
 import  unittest
 from contact1 import Contact1
+from Contactapp import Contact
 
 def is_alert_present(wd):
     try:
@@ -13,108 +14,20 @@ def is_alert_present(wd):
 
 class add_new_contact(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+        self.app=Contact()
     
     def test_add_new_contact(self):
-
-        wd = self.wd
-        #open homepage
-        self.open_homepage(wd)
-        #login
-        self.login(wd, name="admin", password="secret")
-        # add new contact
-        self.addnew_contact(wd, Contact1(fname="Marta", mname="Vu", lname="Ko", nname="no", title="no", compname="Google",
+        self.app.open_homepage()
+        self.app.login(name="admin", password="secret")
+        self.app.addnew_contact(Contact1(fname="Marta", mname="Vu", lname="Ko", nname="no", title="no", compname="Google",
                             address="no", hphone="9999", mphone="4444", wphone="6666", faxnumber="no",
                             e_mail="ya@gmail.com", e_mail2="no", e_mail3="no", home_page="no", birth_year="1980",
                             address_2="no", notes="no"))
-        #logout
-        self.logout(wd)
+        self.app.logout()
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def addnew_contact(self, wd, contact1):
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact1.fname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact1.mname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact1.lname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact1.nname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact1.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact1.compname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact1.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact1.hphone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact1.mphone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact1.wphone)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact1.faxnumber)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact1.e_mail)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact1.e_mail2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact1.e_mail3)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact1.home_page)
-        wd.find_element_by_name("theform").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[19]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[19]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[12]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[12]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact1.birth_year)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact1.address_2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact1.notes)
-        wd.find_element_by_id("content").click()
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def login(self, wd, name="admin", password="secret"):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(name)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_homepage(self, wd):
-        wd.get("http://localhost/addressbook/index.php")
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
 if __name__ == '__main__':
     unittest.main()
