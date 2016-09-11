c17f1b280f00a9be4fb7a0d5615c3201de64553c
mari44ka <marikostenko83@gmail.com>
2016-09-10 22:55:38 -0700

    creating new methods  and new test modify first group
    new methods - 1.select new group
    2. feel_group_form
    3.change field_value
    changed class Group

--- fixture/group.py
+++ fixture/group.py
@@ -8,41 +8,47 @@ class Grouphelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
+        self.feel_group_form(group)
         wd.find_element_by_name("submit").click()
+
+    def feel_group_form(self, group):
+        wd=self.app.wd
+        self.change_field_value("group_name",group.name)
+        self.change_field_value("group_footer", group.footer)
+        self.change_field_value("group_header", group.header)
+
+
+
+
+    def change_field_value(self,field_name,text):
+        wd=self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+
 
     def del_first_group(self):
         wd=self.app.wd
         self.app.open_homepage()
         wd.find_element_by_link_text("groups").click()
         # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         #delete first group
         wd.find_element_by_name("delete").click()
 
-    def edit_first_group(self,group):
+    def select_first_group(self):
+        wd=self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def edit_first_group(self,new_group_data):
         wd = self.app.wd
         self.app.open_homepage()
         wd.find_element_by_link_text("groups").click()
         # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
+        self.feel_group_form(new_group_data)
         wd.find_element_by_name("update").click()
 
--- test/test_editing_first_group.py
+++ test/test_editing_first_group.py
@@ -4,6 +4,8 @@ from model.group import Group
 def test_edit_first_group(app):
     app.open_homepage()
     app.sessio.login(name="admin", password="secret")
-    app.group.edit_first_group(Group(name="ok", footer="ok", header="ok"))
+    app.group.edit_first_group(Group(name="Hallo"))
     app.sessio.logout()
+
+
 
