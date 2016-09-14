962399a9fe7ef4f8e0c615817586002cfbd298ce
mari44ka <marikostenko83@gmail.com>
2016-09-14 00:00:54 -0700

    editing with new data

--- test/test_editing_first_contact.py
+++ test/test_editing_first_contact.py
@@ -1,6 +1,6 @@
-from model.contact1 import Contact11
+from model.contact1 import Contact1
 
 def test_edit_first_contact(app):
     app.open_homepage()
-    app.contact.edit_first_contact(Contact11(lname="Lo", hphone="7777",e_mail2="KK@mail.ru"))
+    app.contact.edit_first_contact(Contact1(lname="Lo", hphone="7777",e_mail2="KK@mail.ru",notes="check"))
 
