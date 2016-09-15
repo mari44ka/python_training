b78493d6a176eb102f40b8d8dc9656aaecb7f045
mari44ka <marikostenko83@gmail.com>
2016-09-14 23:42:15 -0700

    modifying editing_first_group test
    adding condition when there is no group in the address book

--- test/test_editing_first_group.py
+++ test/test_editing_first_group.py
@@ -2,7 +2,8 @@
 from model.group import Group
 
 def test_edit_first_group(app):
-    app.open_homepage()
+    if app.group.count()==0:
+        app.group.creat(Group(name="Test1",footer="Test2"))
     app.group.edit_first_group(Group(name="Hallo"))
 
 
