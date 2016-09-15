df958d43c0b7c1ec3b466def249930b896108574
mari44ka <marikostenko83@gmail.com>
2016-09-14 23:47:33 -0700

    modifying edit_first_contact test
    adding a condition when there is no contact

--- test/test_editing_first_contact.py
+++ test/test_editing_first_contact.py
@@ -2,5 +2,7 @@ from model.contact1 import Contact1
 
 def test_edit_first_contact(app):
     app.open_homepage()
+    if app.contact.count()==0:
+        app.contact.addnew(Contact1(fname="Helen", mname="Chao", lname="Chi",mphone="3435475", wphone="12345", e_mail="" ))
     app.contact.edit_first_contact(Contact1(lname="Lo", hphone="7777",e_mail2="KK@mail.ru",notes="check"))
 
