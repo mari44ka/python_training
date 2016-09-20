50bd76d67903a76f5188028c34048f639dd80b87
mari44ka <marikostenko83@gmail.com>
2016-09-19 23:50:12 -0700

    verification added to edit_first_group test

--- test/test_editing_first_group.py
+++ test/test_editing_first_group.py
@@ -4,7 +4,11 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count()==0:
         app.group.creat(Group(name="Test1",footer="Test2"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="Hallo"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)== len(new_groups)
+
 
 
 
