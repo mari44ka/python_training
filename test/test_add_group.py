8786a76b0b833b661e2f35e8565034c2cb2ff255
mari44ka <marikostenko83@gmail.com>
2016-09-19 23:39:07 -0700

    verification added to the add_group test

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,10 +5,17 @@ from model.group import Group
 
 def test_add_group(app):
         app.open_homepage()
+        old_groups=app.group.get_group_list()
         app.group.creat(Group(name="my", footer="my", header="my"))
+        new_groups=app.group.get_group_list()
+        assert len(old_groups)+1==len(new_groups)
 
 
 def test_add_group_empty(app):
         app.open_homepage()
+        old_groups = app.group.get_group_list()
         app.group.creat(Group(name="", footer="", header=""))
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
+
 
