002b9cc41dde5d74b858c4b79ebe27b6e05f9e95
mari44ka <marikostenko83@gmail.com>
2016-09-19 23:49:46 -0700

    verification added to del_group test

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -4,4 +4,7 @@ from model.group import Group
 def test_del_first_group(app):
     if app.group.count()==0:
         app.group.creat(Group(name="Test1"))
+    old_groups = app.group.get_group_list()
     app.group.del_first_group()
+    new_groups=app.group.get_group_list()
+    assert len(old_groups)-1==len(new_groups)
