4cf2f9d00568be3179ba367cc3d6930eb7a6b3fd
mari44ka <marikostenko83@gmail.com>
2016-09-22 23:13:56 -0700

    new assert added to the tests add_group and edit_group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,19 +3,27 @@
 from model.group import Group
 
 
+
+
 def test_add_group(app):
         app.open_homepage()
         old_groups=app.group.get_group_list()
-        app.group.creat(Group(name="my", footer="my", header="my"))
+        group=Group(name="my", footer="my", header="my")
+        app.group.creat(group)
         new_groups=app.group.get_group_list()
         assert len(old_groups)+1==len(new_groups)
+        old_groups.append(group)
+        assert sorted(old_groups,key=Group.id_or_max)==sorted(new_groups,key=Group.id_or_max)
 
 
 def test_add_group_empty(app):
         app.open_homepage()
         old_groups = app.group.get_group_list()
-        app.group.creat(Group(name="", footer="", header=""))
+        group=Group(name="", footer="", header="")
+        app.group.creat(group)
         new_groups = app.group.get_group_list()
         assert len(old_groups) + 1 == len(new_groups)
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
--- test/test_editing_first_group.py
+++ test/test_editing_first_group.py
@@ -5,9 +5,13 @@ def test_edit_first_group(app):
     if app.group.count()==0:
         app.group.creat(Group(name="Test1",footer="Test2"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="Hallo"))
+    group=Group(name="Hallo")
+    group.id=old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups)== len(new_groups)
+    old_groups[0]=group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
