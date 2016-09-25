38d7435b94de655fb526b08bb7e8a013aa867c2f
mari44ka <marikostenko83@gmail.com>
2016-09-25 00:08:38 -0700

    assertion added to the del_contact and edit_first_contact test by length of list

--- test/test_del_first_contact.py
+++ test/test_del_first_contact.py
@@ -1,10 +1,15 @@
 from model.contact1 import Contact1
 
 def test_del_first_contact(app):
-    app.open_homepage()
+    #app.open_homepage()
+    old_contact=app.contact.get_contact_list()
     if app.contact.count()==0:
         app.contact.addnew(Contact1(fname="Helen", mname="Chao", lname="Chi",mphone="3435475", wphone="12345", e_mail="" ))
     app.contact.del_first_contact()
+    new_contact=app.contact.get_contact_list()
+    assert len(old_contact)-1==len(new_contact)
+    #old_contact[0:1]=[]
+    #assert old_contact==new_contact
 
 
 
--- test/test_editing_first_contact.py
+++ test/test_editing_first_contact.py
@@ -1,8 +1,10 @@
 from model.contact1 import Contact1
 
 def test_edit_first_contact(app):
-    app.open_homepage()
+    #app.open_homepage()
+    old_contact=app.contact.get_contact_list()
     if app.contact.count()==0:
         app.contact.addnew(Contact1(fname="Helen", mname="Chao", lname="Chi",mphone="3435475", wphone="12345", e_mail="" ))
     app.contact.edit_first_contact(Contact1(lname="Lo", hphone="7777",e_mail2="KK@mail.ru",notes="check"))
-
+    new_contact=app.contact.get_contact_list()
+    assert len(old_contact)==len(new_contact)
