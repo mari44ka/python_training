d448f17e7097a7dd7e5811ef1ad2c40c2accf2e3
mari44ka <marikostenko83@gmail.com>
2016-10-16 22:55:22 -0700

    changes

--- test/test_home_page.py
+++ test/test_home_page.py
@@ -6,6 +6,7 @@ def test_home_page(app):
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_homepage.all_phones_from_home_page == app.contact.merge_phones_like_on_home_page(
         contact_from_edit_page)
+    assert contact_from_homepage.fname==contact_from_edit_page.fname
     assert contact_from_homepage.lname == contact_from_edit_page.lname
     assert contact_from_homepage.address == contact_from_edit_page.address
     assert contact_from_homepage.all_emails_from_homepage == app.contact.merge_emails_like_on_home_page(contact_from_edit_page)
