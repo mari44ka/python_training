0f07c4e56bb3fc7887b934d4f4d4eee54fc39583
mari44ka <marikostenko83@gmail.com>
2016-10-17 22:06:17 -0700

    randomization

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,29 +6,26 @@ import random
 import string
 
 def random_string(prefix,maxlen):
-    symbols=string.ascii_letters + string.digits + " "*10
+    symbols=string.ascii_letters + string.digits + string.punctuation +  ""*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata =[
-    Group(name=name, footer=footer, header=header)
-    for name in [" ",random_string("name",10)]
-    for footer in [" ",random_string("footer",20)]
-    for header in [" ",random_string("header",20)]
+testdata =[Group(name="", footer="", header="")] + [
+    Group(name=random_string("name",10), header=random_string("header",20),footer=random_string("footer",20))
+    for i in range(5)
 
 ]
 @pytest.mark.parametrize("group",testdata,ids=[repr(x) for x in testdata])
 
 def test_add_group(app,group):
-        pass
 
-        #app.open_homepage()
-        #old_groups=app.group.get_group_list()
-        #app.group.creat(group)
-        #assert len(old_groups)+1==app.group.count()
-        #new_groups = app.group.get_group_list()
-        #old_groups.append(group)
-        #assert sorted(old_groups,key=Group.id_or_max)==sorted(new_groups,key=Group.id_or_max)
+    app.open_homepage()
+    old_groups=app.group.get_group_list()
+    app.group.creat(group)
+    assert len(old_groups)+1==app.group.count()
+    new_groups = app.group.get_group_list()
+    old_groups.append(group)
+    assert sorted(old_groups,key=Group.id_or_max)==sorted(new_groups,key=Group.id_or_max)
 
 
 #def test_add_group_empty(app):
