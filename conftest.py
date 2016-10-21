ed0755483f3cd4c5c06c456ac2d719d693739457
mari44ka <marikostenko83@gmail.com>
2016-10-20 23:44:58 -0700

    getting information about application from config file; setting path to config file

--- conftest.py
+++ conftest.py
@@ -1,22 +1,25 @@
 
 import pytest
 from fixture.Application import Application
-
+import json
+import os.path
 fixture=None
+target=None
 
 @pytest.fixture()
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--base_url")
-    if fixture is None:
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target=json.load(f)
 
-        fixture=Application(browser=browser,base_url=base_url)
+    if fixture is None or not fixture.is_valid():
 
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser,base_url=base_url)
-    fixture.sessio.ensure_login(name="admin", password="secret")
+        fixture=Application(browser=browser,base_url=target["base_url"])
+    fixture.sessio.ensure_login(name=target["username"], password=target["password"])
     return fixture
 
 @pytest.fixture(scope="session",autouse=True)  #fixture will play automaticly after using autouse
@@ -30,4 +33,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser",action="store",default="firefox")
-    parser.addoption("--base_url",action="store",default="http://localhost/addressbook/index.php")
+    parser.addoption("--target",action="store",default="target.json")
